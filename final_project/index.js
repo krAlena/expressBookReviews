66364161279bd42b89611e7e8b0340e536eb92fb
krAlena <krasnoshchoka@bk.ru>
2024-07-28 22:11:19 +0300

    Promises or Async-Await

--- final_project/index.js
+++ final_project/index.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
+const axios = require('axios').default;
 const app = express();
 
 app.use(express.json());
@@ -35,3 +35,22 @@ app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
+
+const connectToURL = async(url)=>{
+    const outcome = axios.get(url);
+    let result = (await outcome).data;
+
+    return  result;
+}
+
+const urlBase = 'https://zubrytskaaly-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai';
+setTimeout(() => {
+    //Task 10: get books list
+    connectToURL(urlBase + '/').then(result => console.log(result))
+    //Task 11: get book by isbn
+    connectToURL(urlBase + '/isbn/9780714503844').then(result => console.log(result))
+    //Task 12: get book by author
+    connectToURL(urlBase + '/author/Dante Alighieri').then(result => console.log(result))
+    //Task 13: get book by title
+    connectToURL(urlBase + '/title/Fairy tales').then(result => console.log(result))
+}, 5000)
